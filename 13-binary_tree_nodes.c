d9c80f65e32069f9b9d598079c4055e06c5fa838
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-08-01 09:54:35 +0000

    13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - nodes
+ * @tree: t
+ *
+ * Return: know
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
