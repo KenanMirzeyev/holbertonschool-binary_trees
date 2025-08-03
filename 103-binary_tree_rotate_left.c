17fb43f1fabf441ea3251a429ca749c8bfb59bad
Hasanali Qadirov <heseneli536@gmail.com>
2025-08-03 15:53:36 +0400

    new commit

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,10 +8,14 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *right = tree->right;
+	if (tree && tree->right)
+	{
+		binary_tree_t *right = tree->right;
 
-	tree->right = right->left;
-	right->left = tree;
-	right->parent = NULL;
-	return (right);
+		tree->right = right->left;
+		right->left = tree;
+		right->parent = NULL;
+		return (right);
+	}
+	return (NULL);
 }
