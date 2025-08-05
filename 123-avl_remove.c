bd322d3b3077a1a65b1637fa574e5a6e096a14ab
Hasanali Qadirov <heseneli536@gmail.com>
2025-08-05 15:36:03 +0400

    Big O AVL

---
+++ 123-avl_remove.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * avl_remove - remove a node from avl
+ * @root: root node
+ * @value: the value to be removed
+ *
+ * Return: new root node
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+
+}
